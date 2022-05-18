1e7f1e8fae809ba24520d1d0fe5804c766159d31
User Macbook <usermacbook@mbp-de-user-3.home>
2022-05-18 17:54:52 +0000

    fix bug on the upload

---
+++ .history/resources/views/dashboard.blade_20220517192208.php
@@ -0,0 +1,38 @@
+<x-app-layout>
+    <x-slot name="header">
+    </x-slot>
+    <table class="table">
+        <thead class="thead-primary">
+            <tr>
+                <th scope="col">Nom du fichier</th>
+                <th scope="col">Uploader</th>
+                <th scope="col">Taille(ko)</th>
+                <th scope="col">Date d'upload</th>
+                <th scope="col">Téléchagement</th>
+              </tr>
+        </thead>
+        <tbody>
+
+        
+            @foreach ($fichiers as $fichier)
+                <tr>
+                    <th scope="row">{{$fichier->nom_fichier}}</th>
+                    <td>{{$fichier->name}}</td>
+                    <td>{{$fichier->size_fichier}}</td>
+                    <td>{{$fichier->created_at}}</td>
+                    <td><a href=" {{url('/download', $fichier->url_fichier)}} " class="btn btn-success">Télécharger</a></td>
+                </tr>
+                {{-- <div class="fichier_recup">
+                    <p>
+                        <a href=" {{url('/download', $fichier->url_fichier)}} ">{{$fichier->nom_fichier}}</a>
+                        <span>
+                            <i>Uploader par: {{$fichier->name}}</i>
+                            <i>le {{$fichier->created_at}}</i>
+                            <i>Taille {{$fichier->size_fichier}} ko</i>
+                        </span> <br>
+                    </p>    
+                </div> --}}
+            @endforeach
+    </tbody>
+    </table>
+</x-app-layout>
