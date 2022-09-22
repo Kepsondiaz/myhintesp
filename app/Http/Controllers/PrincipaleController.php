c0109bfafaa7e9791a5fbc99d4e0455a83ace22b
Kepsondiaz <kherabadiaby328@gmail.com>
2022-09-22 07:57:47 +0000

    modification sur la requête de selection des fichiers

--- app/Http/Controllers/PrincipaleController.php
+++ app/Http/Controllers/PrincipaleController.php
@@ -13,18 +13,19 @@ class PrincipaleController extends Controller
 {
         public function index(Request $request)
         {
-             $tmp_fichiers = $tmp_fichiers = tmp_fichiers::with('matieres.filieres.departements')->where('valider', 1)->orderby('created_at', 'desc')->paginate(15); 
+             $tmp_fichiers = tmp_fichiers::with('matieres.filieres.departements')->where('valider', 1)->orderby('created_at', 'desc')->paginate(15); 
              return view('dashboard', compact('tmp_fichiers'));
         }
 
         public function search(Request $request)
         {
-                $name = $request->search;
-               $fichiers_search = DB::table('tmp_fichiers')
-               ->join('matieres', 'matieres.id', '=', 'tmp_fichiers.matiere_id')
-               ->select('matieres.nom_matiere', 'matieres.semestres', 'tmp_fichiers.*')
-               ->where('tmp_nom_fichier', 'like', $name)
-               ->paginate(2);
+                $name_search = $request->search;
+        //        $fichiers_search = DB::table('tmp_fichiers')
+        //        ->join('matieres', 'matieres.id', '=', 'tmp_fichiers.matiere_id')
+        //        ->select('matieres.nom_matiere', 'matieres.semestres', 'tmp_fichiers.*')
+        //        ->where('tmp_nom_fichier', 'like', $name)
+        //        ->paginate(2);
+                $fichiers_search = tmp_fichiers::with('matieres.filieres.departements')->where('tmp_nom_fichier', $name_search)->orderby('created_at', 'desc')->paginate(15); 
                 return view('search', compact('fichiers_search'));  
         
         }
