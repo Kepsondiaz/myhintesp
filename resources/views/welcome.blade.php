66b13a6879b5cde08103d241792ed1d16a52978b
Kepsondiaz <kherabadiaby328@gmail.com>
2022-09-22 08:06:12 +0000

    modification de la view welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -49,34 +49,7 @@
     </div>
 </header>
     
-<div class="center">
-    <table class="table">
-        <thead class="thead-primary">
-            <tr>
-                <th scope="col">Nom du fichier</th>
-                <th scope="col">Matieres</th>
-                <th scope="col">Semestres</th>
-                <th scope="col">Niveau</th>
-                <th scope="col">Filieres</th>
-                <th scope="col">Departement</th>
-            </tr>
-        </thead>
-        <tbody>
 
-             @foreach ($tmp_fichiers as $tmp_fichier)
-                <tr>
-                    <td>{{$tmp_fichier->tmp_nom_fichier}}</td>
-                    <td>{{$tmp_fichier->matieres->nom_matiere}}</td>
-                    <td>{{$tmp_fichier->matieres->semestres}}</td>
-                    <td>{{$tmp_fichier->matieres->niveau_matiere}}</td>
-                    <td>{{$tmp_fichier->matieres->filieres->intitule}}</td>
-                    <td>{{$tmp_fichier->matieres->filieres->departements->nom}}</td>
-                </tr>
-            @endforeach 
-    </tbody>
-    </table>
-    <span>{{ $tmp_fichiers->links()}}</span> 
-</div>
 
 
    
