ae20c2e69da49df8903983b382e24f63d1d6c24f
Kepsondiaz <kherabadiaby328@gmail.com>
2022-09-24 09:43:46 +0000

    Controller pour différencié les les fichiers des departements

---
+++ app/Http/Controllers/DepartementsFichiers.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use PhpParser\Node\Expr\FuncCall;
+
+class DepartementsFichiers extends Controller
+{
+    public function genieInfo()
+    {
+        return "salut";
+    }
+
+    public function genieMeca()
+    {
+        return "salut";
+    }
+
+    public function genieElec()
+    {
+        return "salut";
+    }
+
+    public function genieCivil()
+    {
+        return "salut";
+    }
+
+    public function gcba()
+    {
+        
+        return "salut";
+    }
+
+    public function gestion()
+    {
+        return "salut";
+    }
+}
