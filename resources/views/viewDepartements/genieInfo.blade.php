3b5dd176950da8596b95d4c63167e008adac704f
kepsonDiaz <kherabadiaby328@gmail.com>
2022-10-07 09:44:12 +0000

    fixe small error

--- resources/views/viewDepartements/genieInfo.blade.php
+++ resources/views/viewDepartements/genieInfo.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="header">
     </x-slot>
     <div class="container h-100">
-        <h1>Site en construction</h1>
+        <h1>Page en construction</h1>
         <img src="https://login.mailgun.com/login/static/dev-ilustration.png" >
     </div>
 </x-app-layout>
