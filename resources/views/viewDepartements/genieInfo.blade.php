2bbb8a7d4219bb117321f3166de9a1d4ab070ae2
kepsonDiaz <kherabadiaby328@gmail.com>
2022-10-07 09:40:51 +0000

    retrun img for page under construction

---
+++ resources/views/viewDepartements/genieInfo.blade.php
@@ -0,0 +1,8 @@
+<x-app-layout>
+    <x-slot name="header">
+    </x-slot>
+    <div class="container h-100">
+        <h1>Site en construction</h1>
+        <img src="https://login.mailgun.com/login/static/dev-ilustration.png" >
+    </div>
+</x-app-layout>
