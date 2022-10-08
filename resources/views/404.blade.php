aabba432f8f42dbbecba75c9132bcc5f279500b0
kepsonDiaz <kherabadiaby328@gmail.com>
2022-10-08 15:15:54 +0000

    customize 404 page

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -1,3 +1,10 @@
 <x-app-layout>
-    <p>cette page n'existe pas</p>
+    <center class="">
+        <div class="position-absolute start-50 translate-middle-x">
+            <p>
+                <h1>myhintesp</h1>
+                <img src="https://img.icons8.com/external-smashingstocks-flat-smashing-stocks/2x/external-404-Error-web-hosting-and-cloud-services-smashingstocks-flat-smashing-stocks.png" width="500px" alt="">
+            </p> 
+        </div>
+    </center>
 </x-app-layout>    
