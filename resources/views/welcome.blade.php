2001d47bab7be00346c2e14a29be4aa0fd49f321
kepsonDiaz <kherabadiaby328@gmail.com>
2022-10-08 11:25:24 +0000

    deseable login and register options

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,7 +31,7 @@
     <div class="container-fluid fixed-top p-4">
         <div class="col-12">
             <div class="d-flex justify-content-end">
-                @if (Route::has('login'))
+                {{-- @if (Route::has('login'))
                     <div class="">
                         @auth
                             <a href="{{ url('/dashboard') }}" class="text-muted">Dashboard</a>
@@ -43,8 +43,17 @@
                             @endif
                         @endif
                     </div>
-                @endif
+                @endif --}}
             </div>
+            <center class="top-100">
+                <div class="position-absolute top-100 start-50 translate-middle-x">
+                    <p>
+                        <h1>myhintesp</h1>
+                        <img src="https://img.icons8.com/sf-regular-filled/2x/compare.png" width="500px" alt="">
+                    </p> 
+                    <h4><a href="{{url('/dashboard')}}">Commencer à Partager 😉</a></h4>
+                </div>
+            </center>
         </div>
     </div>
 </header>
