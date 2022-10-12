eb612ca15100b9c79e73f97896085cd1bd0352a2
kepsonDiaz <kherabadiaby328@gmail.com>
2022-10-12 02:09:04 +0000

    add filter functionalite

--- app/Http/Controllers/PrincipaleController.php
+++ app/Http/Controllers/PrincipaleController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-
+use App\Models\matieres;
 use App\Models\tmp_fichiers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -20,14 +20,85 @@ class PrincipaleController extends Controller
 
         public function search(Request $request)
         {
-                $name_search = $request->search;
-        //        $fichiers_search = DB::table('tmp_fichiers')
-        //        ->join('matieres', 'matieres.id', '=', 'tmp_fichiers.matiere_id')
-        //        ->select('matieres.nom_matiere', 'matieres.semestres', 'tmp_fichiers.*')
-        //        ->where('tmp_nom_fichier', 'like', $name)
-        //        ->paginate(2);
-                $fichiers_search = tmp_fichiers::with('matieres.filieres.departements')->where('tmp_nom_fichier', $name_search)->orderby('created_at', 'desc')->paginate(15); 
-                return view('search', compact('fichiers_search'));  
+                if($request->departement)
+                {
+                        $fichiers_search = DB::table('tmp_fichiers')
+                               ->join('matieres', 'matieres.id', '=', 'tmp_fichiers.matiere_id')
+                               ->join('filieres', 'filieres.id', '=', 'matieres.filiere_id')
+                               ->join('departements', 'departements.id', '=', 'filieres.departement_id')
+                               ->select('departements.*','filieres.*','matieres.*', 'tmp_fichiers.*')
+                               ->where('departements.nom', $request->departement)
+                               ->where('valider', 1)
+                               ->paginate(2);
+                        //        dd($fichiers_search);
+                               return view('search', compact('fichiers_search'));
+                               
+                }
+                if($request->filieres)
+                {
+                        $fichiers_search = DB::table('tmp_fichiers')
+                        ->join('matieres', 'matieres.id', '=', 'tmp_fichiers.matiere_id')
+                        ->join('filieres', 'filieres.id', '=', 'matieres.filiere_id')
+                        ->join('departements', 'departements.id', '=', 'filieres.departement_id')
+                        ->select('departements.*','filieres.*','matieres.*', 'tmp_fichiers.*')
+                        ->where('filieres.intitule', $request->filieres)
+                        ->where('valider', 1)
+                        ->paginate(2);
+                        return view('search', compact('fichiers_search'));
+                }
+                if($request->matieres)
+                {
+                        $fichiers_search = DB::table('tmp_fichiers')
+                        ->join('matieres', 'matieres.id', '=', 'tmp_fichiers.matiere_id')
+                        ->join('filieres', 'filieres.id', '=', 'matieres.filiere_id')
+                        ->join('departements', 'departements.id', '=', 'filieres.departement_id')
+                        ->select('departements.*','filieres.*','matieres.*', 'tmp_fichiers.*')
+                        ->where('matieres.nom_matiere', $request->matieres)
+                        ->where('valider', 1)
+                        ->paginate(2);
+                        return view('search', compact('fichiers_search'));
+                }
+                if($request->niveaux)
+                {
+                        $fichiers_search = DB::table('tmp_fichiers')
+                        ->join('matieres', 'matieres.id', '=', 'tmp_fichiers.matiere_id')
+                        ->join('filieres', 'filieres.id', '=', 'matieres.filiere_id')
+                        ->join('departements', 'departements.id', '=', 'filieres.departement_id')
+                        ->select('departements.*','filieres.*','matieres.*', 'tmp_fichiers.*')
+                        ->where('matieres.niveau_matiere', $request->niveaux)
+                        ->where('valider', 1)
+                        ->paginate(2);
+                        return view('search', compact('fichiers_search')); 
+                }
+                if($request->semestres)
+                {
+                        $fichiers_search = DB::table('tmp_fichiers')
+                        ->join('matieres', 'matieres.id', '=', 'tmp_fichiers.matiere_id')
+                        ->join('filieres', 'filieres.id', '=', 'matieres.filiere_id')
+                        ->join('departements', 'departements.id', '=', 'filieres.departement_id')
+                        ->select('departements.*','filieres.*','matieres.*', 'tmp_fichiers.*')
+                        ->where('matieres.semestres', $request->semestres)
+                        ->where('valider', 1)
+                        ->paginate(2);
+                        return view('search', compact('fichiers_search')); 
+                }
+                // if($request->departement && $request->filieres && $request->matieres && $request->niveaux && $request->semestres)
+                // {
+                //         $fichiers_search = DB::table('tmp_fichiers')
+                //         ->join('matieres', 'matieres.id', '=', 'tmp_fichiers.matiere_id')
+                //         ->join('filieres', 'filieres.id', '=', 'matieres.filiere_id')
+                //         ->join('departements', 'departements.id', '=', 'filieres.departement_id')
+                //         ->select('departements.*','filieres.*','matieres.*', 'tmp_fichiers.*')
+                //         ->where('valider', 1)
+                //         ->where('departements.nom', $request->departement)
+                //         ->where('filieres.intitule', $request->filieres)
+                //         ->where('matieres.nom_matiere', $request->matieres)
+                //         ->where('matieres.niveau_matiere', $request->niveaux)
+                //         ->where('matieres.semestres', $request->semestres)
+                //         ->paginate(2);
+                //         dd($fichiers_search);
+                //         return view('search', compact('fichiers_search'));     
+                // }
         
         }
 
