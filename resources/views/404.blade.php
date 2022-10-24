e9fc07a0e025dba53b15d0c56f6c0f3c621077ef
kepsonDiaz <kherabadiaby328@gmail.com>
2022-10-24 12:43:12 +0000

    change image for 404 error page

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -3,8 +3,11 @@
         <div class="position-absolute start-50 translate-middle-x">
             <p>
                 <h1>myhintesp</h1>
-                <img src="https://img.icons8.com/external-smashingstocks-flat-smashing-stocks/2x/external-404-Error-web-hosting-and-cloud-services-smashingstocks-flat-smashing-stocks.png" width="500px" alt="">
+                <img src="/images/error-404.png" width="500px" height="500px" alt="">
             </p> 
+            <p>
+                <a href="{{url("/")}}">Dashboard</a>
+            </p>
         </div>
     </center>
 </x-app-layout>    
